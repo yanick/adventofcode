6862e444afc995f0112cbe36a915e95868e8e53b
Yanick Champoux <yanick@babyl.dyndns.org>
2017-12-04 12:21:06 -0500

    day07

---
+++ 2015/07/wires.pl
@@ -0,0 +1,35 @@
+use 5.20.0;
+use experimental 'signatures';
+
+my %wire;
+
+while(<>) {
+    chomp;
+    my( $instructions, $wire ) = split /\s*->\s*/;
+    $wire{$wire} = $instructions;
+}
+
+say resolve('a');
+
+my %seen;
+sub resolve($key,@cycle) {
+    die "@cycle" if $key ~~ @cycle;
+    push @cycle, $key;
+    my $code = $wire{$key};
+    #warn $code;
+
+    $code =~ s/\b([a-z]+)\b/resolve($1,@cycle)/ge
+        or warn "end of the line $key";
+
+    $code =~ s/NOT/~/;
+    $code =~ s/LSHIFT/<</;
+    $code =~ s/RSHIFT/>>/;
+    $code =~ s/OR/|/;
+    $code =~ s/AND/&/;
+
+    die $code if $code =~ /[A-Z]/;
+
+    warn $key, ' -> ', $code;
+    $wire{$key} = eval( $code ) || 0;
+}
+
