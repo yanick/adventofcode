55fb225898b823c469afa8673752a96d5cb75ffe
Yanick Champoux <yanick@babyl.dyndns.org>
2017-12-05 12:47:11 -0500

    day11

---
+++ 2015/11/1.pl
@@ -0,0 +1,19 @@
+use 5.20.0;
+use experimental 'signatures';
+
+my $alpha = join '', 'a'..'z';
+my $seq = join '|', map { substr $alpha, $_, 3 } 0..length($alpha)-3;
+$seq = qr/$seq/;
+
+
+my $password = <>;
+chomp($password);
+
+1 until is_valid(++$password);
+say $password;
+
+sub is_valid($p) {
+    return 0 if $p =~ /[iol]/;
+    return 0 unless $p =~ /(.)\1.*?(.)\2/;
+    return $p =~ $seq;
+}
