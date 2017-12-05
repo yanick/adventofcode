dee924dce641eda3f833e4885a2173b300761fb6
Yanick Champoux <yanick@babyl.dyndns.org>
2017-12-05 15:12:32 -0500

    day12

---
+++ 2015/12/a.pl
@@ -0,0 +1,20 @@
+use 5.20.0;
+
+use List::AllUtils qw/ sum /;
+use File::Serialize;
+
+use experimental 'signatures', 'smartmatch';
+
+my $data = deserialize_file shift;
+
+say add($data);
+
+sub add($data) {
+   given ( ref $data ) {
+       return sum map { add($_) } @$data when 'ARRAY';
+       return sum map { add($_) } values %$data when 'HASH';
+       default { return 0 + $data };
+   }
+}
+
+
