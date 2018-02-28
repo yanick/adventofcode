7bd44fb896981fc634f5e063cbbf68824fb42e54
Yanick Champoux <yanick@babyl.dyndns.org>
2018-02-28 14:58:20 -0500

    2016-19

--- 2016/19/2.pl
+++ 2016/19/2.pl
@@ -4,9 +4,12 @@ my $input = 3004953;
 
 my @elves = 1..$input;
 
+my $j;
+
 while( @elves > 1 ) {
+    say $elves[0] unless $j++ % 1000;
+    splice @elves, @elves/2, 1;
     push @elves, shift @elves;
-    shift @elves;
 }
 
 say @elves;
