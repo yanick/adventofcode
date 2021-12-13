363ac1c96db6fa8a0fdc38aac97e38bdf2437aa5
Yanick Champoux <yanick@babyl.ca>
2021-12-13 11:03:28 -0500

    prettier

--- 2021/13/part1.mjs
+++ 2021/13/part1.mjs
@@ -2,69 +2,69 @@ import fs from "fs-extra";
 import fp from "lodash/fp.js";
 import _ from "lodash";
 
-export const processInput = (input) => fs.readFile(input,'utf8').then(
-    content => {
-        let [ coords, instructions ] = content.split("\n\n");
+export const processInput = (input) =>
+  fs.readFile(input, "utf8").then((content) => {
+    let [coords, instructions] = content.split("\n\n");
 
-        let grid = [];
+    let grid = [];
 
-        for( const [y,x] of coords.split("\n").map(line => line.split(",").map( x => parseInt(x) )) ) {
-            if(!grid[x]) grid[x] = [];
-            grid[x][y] = true;
-        }
-
-        grid = [...grid];
-        grid = grid.map( x => x? [...x]:[] );
-
-        instructions = instructions.split("\n").filter(x=>x).map( line => line.match(/([xy])=(\d+)/) )
-        .map( ([_,direction,level]) => ([direction,parseInt(level)]) );
-
-        return {
-            grid,
-            instructions,
-        }
-
-    }
-);
-
-export function foldGrid(grid,direction,level) {
-    if(direction === 'x') {
-        for( const row of grid ) {
-            if(!row) continue;
-            const folded = row.splice(level);
-            folded.forEach((v,i) => {
-                if(i===0) return;
-                if(!v) return;
-                row[level-i] = v;
-            })
-        }
-
-        return grid;
+    for (const [y, x] of coords
+      .split("\n")
+      .map((line) => line.split(",").map((x) => parseInt(x)))) {
+      if (!grid[x]) grid[x] = [];
+      grid[x][y] = true;
     }
 
-    const folded = grid.splice(level);
-    folded.forEach((v,i) => {
-        if(i===0) return;
-        if(!v) return;
-        grid[level-i] =
-            _.range(_.max([grid[level-i].length,folded[i].length])).
-                map( j => grid[level-i][j] || folded[i][j] );
-    })
+    grid = [...grid];
+    grid = grid.map((x) => (x ? [...x] : []));
+
+    instructions = instructions
+      .split("\n")
+      .filter((x) => x)
+      .map((line) => line.match(/([xy])=(\d+)/))
+      .map(([_, direction, level]) => [direction, parseInt(level)]);
+
+    return {
+      grid,
+      instructions,
+    };
+  });
+
+export function foldGrid(grid, direction, level) {
+  if (direction === "x") {
+    for (const row of grid) {
+      if (!row) continue;
+      const folded = row.splice(level);
+      folded.forEach((v, i) => {
+        if (i === 0) return;
+        if (!v) return;
+        row[level - i] = v;
+      });
+    }
 
     return grid;
+  }
+
+  const folded = grid.splice(level);
+  folded.forEach((v, i) => {
+    if (i === 0) return;
+    if (!v) return;
+    grid[level - i] = _.range(
+      _.max([grid[level - i].length, folded[i].length])
+    ).map((j) => grid[level - i][j] || folded[i][j]);
+  });
+
+  return grid;
 }
 
 export function printGrid(grid) {
-    return grid.map( line =>
-        (line||[]).map( x => x ? '#':'.' ).join('')
-     ).join("\n");
+  return grid
+    .map((line) => (line || []).map((x) => (x ? "#" : ".")).join(""))
+    .join("\n");
 }
 
-export function solution({grid, instructions}) {
+export function solution({ grid, instructions }) {
+  foldGrid(grid, ...instructions.shift());
 
-    foldGrid(grid,...instructions.shift());
-
-    return _.sum(grid.flat());
-
-
+  return _.sum(grid.flat());
 }
--- 2021/13/part2.mjs
+++ 2021/13/part2.mjs
@@ -2,13 +2,10 @@ import fs from "fs-extra";
 import fp from "lodash/fp.js";
 import _ from "lodash";
 
-import * as p1 from './part1.mjs';
+import * as p1 from "./part1.mjs";
 
-export function solution({grid, instructions}) {
+export function solution({ grid, instructions }) {
+  instructions.forEach((i) => p1.foldGrid(grid, ...i));
 
-    instructions.forEach(i =>p1.foldGrid(grid,...i));
-
-    return p1.printGrid(grid);
-
-
+  return p1.printGrid(grid);
 }
